261061916c817dbb4aefdb240977d50a6715dcd3
KhyllePagong <159061500+KhyllePagong@users.noreply.github.com>
2024-03-15 11:54:58 -0600

    added a timer

--- Task assignment/sketch.js
+++ Task assignment/sketch.js
@@ -1,29 +1,40 @@
 let buttonPressed = false;
 let myButton = {x: 0, y: 0, w: 0, h: 0, color: ""}
-
+let x = 0;
+let y = 0;
+let z = 0;
+let t=2000;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   myButton.w = 120;
   myButton.h = 40;
   myButton.x = width/2 - myButton.w/2;
-  myButton.y = height/2 - myButton.h/2;
+  myButton.y = height/9 - myButton.h/2;
   myButton.color = "red";
 }
 
 function draw() {
-  background(220);
-
+  background(0);
   if (buttonPressed===false) {
     drawButton();
   }
+  else {
+    character();
+    myButton.color=(0,255,150);
+    drawButton();
+    for (let i=0; i < myButton.length; i++){
+      if (millis() > t){
+        buttonPressed=false;
+        let t=math.Floor(t*2);
+      }
+    }
+  }
 }
-
-
 function mousePressed() {
 
   if (mouseX > myButton.x && mouseX < (myButton.x + myButton.w)) {
-    if(true) {
+    if(mouseY > myButton.y && mouseY < (myButton.y + myButton.h)) {
       buttonPressed = true;
     }
     
@@ -33,4 +44,26 @@ function mousePressed() {
 function drawButton() {
   fill(myButton.color);
   rect(myButton.x, myButton.y, myButton.w, myButton.h);
+}
+function character() {
+  let a = 0;
+  background(220);
+  noStroke();
+  rotate(a);
+  fill(0,255,150);
+  rect(width/4,height/4,300,300);
+  fill(0);
+  rect(width/4,height/3,100,100);
+  rect(width/1.96,height/3,100,100);
+  fill(255);
+  rect(width/4,height/3,50,50);
+  rect(width/1.96,height/3,50,50);
+  fill(255,0,0);
+  rect(width/3.2,height/2,200,45);
+  fill(0,255,150);
+  rect(width/2,height/1.2,50,100);
+  rect(width/3.1,height/1.2,50,100);
+  rect(width/1.7,height/1.5,100,50);
+  rect(width/5.5,height/1.5,100,50);
+  circle(width/2.25,height/1.36,250); 
 }
