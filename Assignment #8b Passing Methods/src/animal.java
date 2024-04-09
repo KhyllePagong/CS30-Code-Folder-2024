c6a8d6deea70a82fc478fcf2c63ccbf20c2a350f
KhyllePagong <159061500+KhyllePagong@users.noreply.github.com>
2024-04-09 11:11:24 -0600

    made two metnhods and outpuits it

--- Assignment #8b Passing Methods/src/animal.java
+++ Assignment #8b Passing Methods/src/animal.java
@@ -3,20 +3,9 @@ import java.util.Scanner;
 
 public class animal {
     public static void main(String[] args) throws Exception{
-        String type, answer;
-
-        Scanner keyboard = new Scanner (System.in);
-
         animalrunner animals = new animalrunner();
-
-        System.out.println("Dog or Cat? :)");
-
-        type=keyboard.nextLine();
-        answer=type.toUpperCase();
-
-        if (answer=="CAT"){
-            animals.cat();
-        }
-        System.out.println(answer);
+        animals.cat();
+        animals.dog();
     }
 }
+
