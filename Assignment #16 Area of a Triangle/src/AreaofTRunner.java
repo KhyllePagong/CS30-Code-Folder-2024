c2ddd1bfe7b1cb7fe70271a6af000354b4f38c6d
KhyllePagong <159061500+KhyllePagong@users.noreply.github.com>
2024-04-17 12:09:05 -0600

    q

--- Assignment #16 Area of a Triangle/src/AreaofTRunner.java
+++ Assignment #16 Area of a Triangle/src/AreaofTRunner.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
 public class AreaofTRunner {
-    
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner (System.in);
+        System.out.print("Please input the sides of the triangle. \na=");
+        int a = keyboard.nextInt();
+        System.out.print("b=");
+        int b = keyboard.nextInt();
+        System.out.print("c=");
+        int c = keyboard.nextInt();
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+
+    }
 }
