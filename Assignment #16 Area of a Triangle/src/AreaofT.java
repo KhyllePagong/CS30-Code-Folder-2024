45c3d3112194ec79abf4be48a9a60510e244932d
KhyllePagong <159061500+KhyllePagong@users.noreply.github.com>
2024-04-18 12:08:40 -0600

    progress on areaopf triangle

--- Assignment #16 Area of a Triangle/src/AreaofT.java
+++ Assignment #16 Area of a Triangle/src/AreaofT.java
@@ -1,5 +1,25 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class AreaofT {  
+    private int SideA, SideB, SideC;
+    private double perimeter;
+    private double area;
     
+    public AreaofT(int a, int b, int c){
+        setSides(0,0,0);
+        perimeter=0;
+        area=0;
+    }
+    public void setSides(int a, int b, int c){
+       SideA = a;
+       SideB = b;
+       SideC = c;
+    }
+    public void calcP(int a, int b, int c){
+        perimeter = a + b + c;
+        System.out.println(perimeter);
+    }
+    public void calcA(){
+
+    }
 }
--- Assignment #16 Area of a Triangle/src/AreaofTRunner.java
+++ Assignment #16 Area of a Triangle/src/AreaofTRunner.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 public class AreaofTRunner {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner (System.in);
+        AreaofT calculate = new AreaofT(0, 0, 0);
         System.out.print("Please input the sides of the triangle. \na=");
         int a = keyboard.nextInt();
         System.out.print("b=");
         int b = keyboard.nextInt();
         System.out.print("c=");
         int c = keyboard.nextInt();
-
-        System.out.print(a);
-        System.out.print(b);
-        System.out.print(c);
-
+        calculate.calcP(a,b,c);
     }
 }
