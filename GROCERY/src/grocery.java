b30324077672493ebef1df284cb919c4e8d2c897
KhyllePagong <159061500+KhyllePagong@users.noreply.github.com>
2024-05-28 12:04:18 -0600

    smthn

--- GROCERY/src/grocery.java
+++ GROCERY/src/grocery.java
@@ -13,9 +13,29 @@ public class grocery {
                 if (yn !=1){
                     System.out.println("Your Grocery list: ");
                     for (int x = 0; x < items.size();x++){
-                        System.out.println(items.get(x));
+                        System.out.println(x+1 + " . "+items.get(x));
                     }
-                    i++;
+                    System.out.print("Press 5 to Update your grocery list \nPress 6 to Remove an item ");
+                    int upd = keyboard.nextInt();
+                    if (upd == 5){
+            
+                    }
+                    else if (upd==6){
+                        System.out.print("Enter the number of the item you would like to remove: ");
+                        int remve = keyboard.nextInt();
+                        items.remove(remve-1);
+                        System.out.print("Enter 1 to replace the removed item ");
+                        int rplce = keyboard.nextInt();
+                        if (rplce == 1){
+                            System.out.println("Replace the removed item with? ");
+                            newitem = keyboard.nextLine();
+                            items.add(remve-1,newitem);
+                        }
+                        System.out.println("Your Grocery list: ");
+                         for (int x = 0; x < items.size();x++){
+                        System.out.println(x+1 + " . "+items.get(x));
+                        }
+                    }
                 }
             }
         }
